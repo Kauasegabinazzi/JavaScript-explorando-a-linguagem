7f12927057f9f349e1cad3d339791611b112e1da
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-12 23:41:29 -0300

    3 - commit

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+console.log('variaveis');
+
+// JS é case sensitive
+
+const year = 29;
+const name = "test";
+let test = 2029
+// sem const ou let  colocar - sobe para o escopo global
+
+console.log(year);
