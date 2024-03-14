553503a4efd2d00fb9d4bbf7389a8909171174c2
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-13 23:38:51 -0300

    five

---
+++ atribuicao.js
@@ -0,0 +1,14 @@
+const names = 'mateus';
+const lastName = 'last';
+
+console.log(names, lastName);
+
+console.log(`meu nome é ${names} ${lastName}` );
+
+const nameComplet = names + lastName;
+// nameComplet = 2;
+console.log(nameComplet);
+
+let cont = 0;
+cont = cont + 1;
+console.log(cont);
