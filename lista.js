9bd3016569309bcd00fdb38caa6387235cfad329
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-14 21:47:48 -0300

    six

---
+++ lista.js
@@ -0,0 +1,18 @@
+let idade; //declarando variavel
+idade = 26; // atribuindo valor
+
+console.log(idade);
+
+const list = new Array(
+    'sp',
+    'rs',
+    'rj'
+);
+
+list.push('rr');
+console.log(list);
+
+list.splice(3,1);
+console.log(list);
+
+console.log(list[2]);
