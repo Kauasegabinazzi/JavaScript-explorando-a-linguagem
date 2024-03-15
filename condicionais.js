72cd1d285ef2819e28b6865eb1f51a7101678b7e
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-15 00:14:40 -0300

    nine

--- condicionais.js
+++ condicionais.js
@@ -4,17 +4,23 @@ const list = new Array(
     'rj'
 );
 const age = 10;
+const estaAcompanhada = true;
 
-if(age >= 18){
-    list.splice(1,1);
+if (age >= 18) {
+    list.splice(1, 1);
 }
-else{
-    console.log('não é maior de idade')
+else if (estaAcompanhada == true) {
+    console.log('está acompanhado');
+    list.splice(1, 1);
 }
+else {
+    console.log('não é maior de idade');
+}
+
 console.log(list);
 
-console.log(age > 18);
-console.log(age < 18);
-console.log(age >= 18);
-console.log(age <= 18);
-console.log(age == 18);
+// console.log(age > 18);
+// console.log(age < 18);
+// console.log(age >= 18);
+// console.log(age <= 18);
+// console.log(age == 18);
