ea8f03ca3e02f2604ce2dff6d6021ab182616b30
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-16 01:21:36 -0300

    eight

--- condicionais.js
+++ condicionais.js
@@ -3,20 +3,34 @@ const list = new Array(
     'rs',
     'rj'
 );
-const age = 10;
+const age = 20;
 const estaAcompanhada = true;
+const pass = true;
+// if (age >= 18) {
+//     list.splice(1, 1);
+// }
+// else if (estaAcompanhada == true) {
+//     console.log('está acompanhado');
+//     list.splice(1, 1);
+// }
+// else {
+//     console.log('não é maior de idade');
+// }
 
-if (age >= 18) {
-    list.splice(1, 1);
-}
-else if (estaAcompanhada == true) {
-    console.log('está acompanhado');
+if (age >= 18 || estaAcompanhada == true) {
     list.splice(1, 1);
 }
 else {
     console.log('não é maior de idade');
 }
 
+console.log("embarque: \n" );
+if(age >= 18 && pass){
+    console.log("boa viagem");
+}
+else{
+    console.log('não pode')
+}
 console.log(list);
 
 // console.log(age > 18);
