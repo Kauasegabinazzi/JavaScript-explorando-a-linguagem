ec8204fb2dfa98f2ef32d9d6698f3b5dfb386323
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-17 00:43:12 -0300

    ten

--- loops.js
+++ loops.js
@@ -6,7 +6,7 @@ const list = new Array(
 const age = 20;
 const estaAcompanhada = true;
 let pass = true;
-const destino = 'rj';
+const destino = 'rs';
 
 console.log('\n destino possiveis');
 console.log(list);
@@ -15,13 +15,14 @@ console.log(list);
 const compra = age >= 18 || estaAcompanhada == true;
 
 let cont = 0;
-
+let destinoexist = false;
 while (cont < 3) {
     if (list[cont] == destino) {
-        console.log('existe')
-    }
-    else{
-        console.log('nao existe')
+        console.log('existe');
+        destinoexist = true;
+        break;
     }
     cont += 1;
 }
+
+console.log('destino: ', destinoexist)
